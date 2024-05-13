ac9d79c9fdc12ff1cf2f08a8edd21e997adc7353
Marcos Gronda <mgronda@itba.edu.ar>
2024-05-13 14:58:49 -0300

    fix: bit operation correction

--- src/main/java/ar/edu/itba/stenography/LSB1.java
+++ src/main/java/ar/edu/itba/stenography/LSB1.java
@@ -10,49 +10,36 @@ public class LSB1 {
     private static final int INT_SIZE = Integer.BYTES * BITS_IN_BYTE;
     private static final int BMP_HEADER_SIZE = 54;
 
-    public static BMPFile hideFile(BMPFile inFile, byte[] fileToHide, String fileExtension){
-        if(inFile.getContentSize() < fileToHide.length * BITS_IN_BYTE + INT_SIZE + (fileExtension.length() + 1) * BITS_IN_BYTE){
+    public static BMPFile hideFile(BMPFile inFile, byte[] fileToHide){
+        if(inFile.getContentSize() < fileToHide.length * BITS_IN_BYTE + INT_SIZE){
             throw new RuntimeException("No tenes suficiente espacio paaaa");
         }
 
         BMPFile outFile = new BMPFile(inFile);
         byte[] outBytes = outFile.getBytes();
 
-        int offset = BMP_HEADER_SIZE;       // TODO: podriamos siempre usar el offset para escribir a outBytes
+        int outBytesPosition = BMP_HEADER_SIZE;
 
         // Ocultamos el tamaño del archivo
         int size = fileToHide.length;
         for(int i=0; i<INT_SIZE; i++){
-            outBytes[offset + i] |= (byte) (size & 1);
+            outBytes[outBytesPosition] &= (byte) (~1 + (size & 1));
             size >>= 1;
-        }
 
-        offset += INT_SIZE;
+            outBytesPosition++;
+        }
 
         // Ocultamos contenido del archivo
         for(int i=0; i<fileToHide.length; i++){
             byte byteToHide = fileToHide[i];
 
             for (int j = 0; j < BITS_IN_BYTE; j++) {
-                outBytes[offset + BITS_IN_BYTE * i + j] |= (byte) (byteToHide & 1);
+                outBytes[outBytesPosition] &= (byte) (~1 + (byteToHide & 1));
                 byteToHide >>= 1;
+
+                outBytesPosition++;
             }
         }
-
-        offset += fileToHide.length * BITS_IN_BYTE;
-
-        // Ocultamos la extension
-        byte[] fileExtensionBytes = generateExtensionArray(fileExtension);
-
-        for(int i=0; i<fileExtension.length(); i++){
-            byte byteToHide = fileExtensionBytes[i];
-
-            for (int j = 0; j < BITS_IN_BYTE; j++) {
-                outBytes[offset + BITS_IN_BYTE * i + j] |= (byte) (byteToHide & 1);
-                byteToHide >>= 1;
-            }
-        }
-
         return outFile;
     }
 
@@ -60,31 +47,22 @@ public class LSB1 {
     public static byte[] obtainFile(BMPFile inFile){
         byte[] inBytes = inFile.getBytes();
 
-        int offset = BMP_HEADER_SIZE;
+        int inBytesOffset = BMP_HEADER_SIZE;
+        int outBytesOffset = 0;
 
         // Para obtener tamaño del file escondido
         int fileSize = 0;
-        for (int i = offset ; i < offset + INT_SIZE; i++){
-            fileSize += (byte) (inBytes[i] & 1);
+        for (int i = 0 ; i < INT_SIZE; i++){
+            fileSize += (byte) (inBytes[inBytesOffset] & 1);
             fileSize <<= 1;
+
+            inBytesOffset++;
         }
         byte[] outBytes = new byte[fileSize];
 
         // TODO: complete
 
         return outBytes;
-    }
-
-
-    private static byte[] generateExtensionArray(String fileExtension) {
-        byte[] fileExtensionBytes = fileExtension.getBytes();
-        byte[] resp = new byte[fileExtensionBytes.length + 1];
-
-        System.arraycopy(fileExtensionBytes, 0, resp, 0, fileExtensionBytes.length);
-
-        resp[resp.length - 1] = '\0';
-
-        return resp;
     }
 
 }
